ad11dd9e1ea0e3469ded68f66b632a1ba30e0498
Sam Bumgardner <Sambumgardner1@gmail.com>
2026-06-06 10:20:52 -0500

    Convert to addon (#2)
    * move generic cutscene code to addons folder
    
    * stagger box, character, name fade in/out
    
    * rename directory for example usage
    
    * basic import plugin
    
    * hard-coded, fragile import example
    
    * Improve automatic pause handling
    
    Don't want to pause if the punctuation is immediately followed by more non-whitespace characters, that means its a numeric decimal point or some such
    
    * fancy import logic completed
    
    * add content to new cutscene to amuse myself
    
    * experimented with more imports
    
    * remove old cutscene resources
    
    * moved all parsing logic to importer
    
    * Make dialogue box background configurable in DialogueCutscene node
    
    * rework box resizing and ninepatchrect change handling
    
    * make dialogue arrow overrideable
    
    * Add dynamic text resize to fit 4 lines into a dialogue box.
    
    May not work entirely correctly, there's some sketchiness in the calculation. The -1 in particular is concerning, but it's probably fine enough.
    
    * make name tag background configurable
    
    * Added simple graphics to the plugin's assets
    
    It's nicer to use than the bad-looking defaults, that's for sure.
    
    * Add method for overriding graphics

---
+++ examples/src/SampleGameplay.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var cutscenes : Array[DialogueCutsceneData] = [
+	preload("res://examples/data/cutscenes/test.dc"),
+	preload("res://examples/data/cutscenes/Cutscene_01.dc"),
+	preload("res://examples/data/cutscenes/Cutscene_02.dc"),
+	preload("res://examples/data/cutscenes/Cutscene_03.dc"),
+]
+var current_cutscene_i = 0
+
+func _ready():
+	_init_gameplay()
+	$CutsceneLayer/DialogueCutscene.cutscene_finished.connect(_on_cutscene_finished)
+	
+	$CutsceneLayer/DialogueCutscene.set_graphic_overrides(
+		$CutsceneLayer/AltDialogueBoxGraphic,
+		$CutsceneLayer/AltNameTagGraphic,
+		preload("res://examples/art/dialogue_box/simple_arrow.png"))
+
+	# These nodes aren't needed anymore, they were just around as a convenient way to define nine-patch backgrounds.
+	$CutsceneLayer/AltDialogueBoxGraphic.queue_free()
+	$CutsceneLayer/AltNameTagGraphic.queue_free()
+
+func _init_gameplay() -> void:
+	var gameplay_tween = create_tween()
+	gameplay_tween.tween_property($GameplayLayer/MockGameplay, "position", Vector2(1000, 325), 3)
+	gameplay_tween.tween_property($GameplayLayer/MockGameplay, "position", Vector2(150, 325), 3)
+	gameplay_tween.set_loops(-1)
+
+func _input(event) -> void:
+	if event.is_action_pressed("ui_accept"):
+		start_cutscene(cutscenes[current_cutscene_i])
+
+func start_cutscene(cutscene : DialogueCutsceneData) -> void:
+	get_tree().paused = true
+	
+	$CutsceneLayer.show()
+	$CutsceneLayer/DialogueCutscene.init_cutscene(cutscene)
+	$CutsceneLayer/DialogueCutscene.open_cutscene()
+
+func _on_cutscene_finished() -> void:
+	current_cutscene_i = (current_cutscene_i + 1) % cutscenes.size()
+	get_tree().paused = false
+	$CutsceneLayer.hide()
